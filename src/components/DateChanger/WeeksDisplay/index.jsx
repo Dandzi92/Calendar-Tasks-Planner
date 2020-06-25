ed4c462dbd274d7671947102b7f4193f3bb43ff4
Dzianis Danilkou <d.danilkou@godeltech.com>
2020-06-26 02:22:06 +0300

    feat: add changer dispaly for week mode

--- src/components/DateChanger/WeeksDisplay/index.jsx
+++ src/components/DateChanger/WeeksDisplay/index.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import moment from 'moment';
 
 const WeeksDisplay = ({ appMoment }) => {
-  return <div>WeeksDisplay</div>;
+  const currentMoment = moment(appMoment);
+  let weekStartDay = currentMoment
+    .clone()
+    .day(1)
+    .format('D MMMM YYYY');
+  let weekEndDay = currentMoment
+    .clone()
+    .add('1', 'weeks')
+    .day(0)
+    .format('D MMMM YYYY');
+  return <div>{`${weekStartDay} - ${weekEndDay}`}</div>;
 };
 
 export default WeeksDisplay;
