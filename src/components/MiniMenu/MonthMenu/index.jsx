3960a369e9c7d7be9581ae29e2e396c438152662
Dzianis Danilkou <d.danilkou@godeltech.com>
2020-06-29 16:11:59 +0300

    feat: add month menu

--- src/components/MiniMenu/MonthMenu/index.jsx
+++ src/components/MiniMenu/MonthMenu/index.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import moment from 'moment';
 import styles from './styles.module.scss';
+import { setMoment } from '../../../features/calendar';
+import { useDispatch } from 'react-redux';
+import ResetMomentButton from '../../ResetButton';
 
 const MonthMenu = ({ appMoment }) => {
-  return <div>MonthMenu</div>;
+  const currentMoment = moment(appMoment);
+  const dispatch = useDispatch();
+
+  return (
+    <div className={styles.container}>
+      <ResetMomentButton />
+      <div className={styles.months}>
+        {[...Array(12)].map((item, index) => {
+          currentMoment.month(index);
+          const timeStamp = currentMoment.format();
+          return (
+            <div
+              className={styles.month}
+              key={index}
+              onClick={() => dispatch(setMoment(timeStamp))}
+            >
+              {currentMoment.format('MMMM')}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default MonthMenu;
